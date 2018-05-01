a1f98b6bbceee1592e40a51b48e66478b6f0f044
megyerimarton <megyerimarton@gmail.com>
2018-05-01 10:34:39 +0200

    lodash

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const { User, validate } = require('../models/user');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
@@ -10,14 +11,16 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered');
 
-  user = new User({
+  /* user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
-  });
+  }); */
+  user = new User(_.pick(req.body, ['name', 'email', 'password'])); // maybe it's more elegant
 
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 
