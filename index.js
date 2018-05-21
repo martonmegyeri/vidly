d03742578c8d3573117b8621544dec3af47b70a3
megyerimarton <megyerimarton@gmail.com>
2018-05-21 15:19:35 +0200

    prod

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 
 const port = process.env.port || 3000;
